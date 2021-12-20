818dbce71223e020ee782785d4d13709867b50ac
horaciodemaio <horaciodemaio33@gmail.com>
2021-12-20 20:09:00 -0300

    cargado de ramas base y compare en la BD

---
+++ _api/contents.js
@@ -0,0 +1,15 @@
+//acá armo las 2 ramas base y compare para luego cargarlas en la BD
+const ArrayBranches = [
+    {
+        name: "base"
+    },
+    {
+        name: "compare"
+    }
+]
+
+module.exports={
+    
+    ArrayBranches
+
+};
--- _api/index.js
+++ _api/index.js
@@ -1,9 +1,17 @@
+const {Branches} = require("./src/db");
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { ArrayBranches } = require("./contents")
 
 conn.sync({ force: true })
 .then(() => {
-  
+  //cargo las ramas base y compare en la BD
+  const Branche = ArrayBranches.map((d) => {
+    return {
+      name: d.name,
+    }
+  });
+  Branches.bulkCreate(Branche).then(console.log("me cargaron los datos en la tabla de Branches"))
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
